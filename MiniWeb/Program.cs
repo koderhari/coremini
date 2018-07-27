84a00e7dbbb8d82fef75bd76b9436bccba16b374
Ilya Zimin <koderhari@gmail.com>
2018-07-27 07:37:35 +0300

    init

---
+++ MiniWeb/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MiniWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           new WebHostBuilder()
+            .UseKestrel()
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.Run(async context =>
+                {
+                    var path = context.Request.Path;
+                    await context.Response.WriteAsync($"Hello! You asked {path}");
+                });
+            });
+    }
+}
