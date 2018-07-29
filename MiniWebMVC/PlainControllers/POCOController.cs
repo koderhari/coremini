a743b2ddc17a2dd30bc1e34079e996599ac3b151
Ilya Zimin <koderhari@gmail.com>
2018-07-29 09:49:55 +0300

    add miniwebmvc and miniwebapi

---
+++ MiniWebMVC/PlainControllers/POCOController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWebMVC.PlainControllers
+{
+    public class POCOController
+    {
+        public IActionResult Http([FromQuery]int p1 = 0)
+        {
+            return new ContentResult(){ Content = p1.ToString()};
+        }
+    }
+}
